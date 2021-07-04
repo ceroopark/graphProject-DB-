8ca66fbc901a5fe9a06609261a1f0866130f20f8
ceroopark <ceroopark@naver.com>
2021-07-04 11:33:14 +0900

    2021.07.04

---
+++ graphProject/MyGraph.java
@@ -0,0 +1,53 @@
+package dbp.project1;
+
+public class MyGraph implements Graph {
+
+	@Override
+	public Vertex addVertex(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Vertex getVertex(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Iterable<Vertex> getVertices() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Iterable<Vertex> getVertices(String key, Object value) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Edge addEdge(Vertex outVertex, Vertex inVertex, String label) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Edge getEdge(Vertex outVertex, Vertex inVertex, String label) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Iterable<Edge> getEdges() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Iterable<Edge> getEdges(String key, Object value) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
